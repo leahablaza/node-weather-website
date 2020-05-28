b6e3aa8264983da1650434502e080587c652ce0e
LeahAblaza <leahamor03@laa@gmail.com>
2020-05-28 14:11:24 +0800

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=474a997a8494d09bfc1f9834a62ed372" +
+    "&query=" +
+    latitude +
+    "," +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.temperature +
+          " degress out. There is a " +
+          body.current.precip +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
